89fb03617485bd7843596eebc11e9f3ee8f1b255
SIGMAGHOST <abdullahsajid1098@gmail.com>
2023-02-05 21:53:34 +0500

    JS Assignments

---
+++ Assignment NO.3/app.js
@@ -0,0 +1,11 @@
+//Assignment NO.3
+
+var age=("17 years old");
+alert(age);
+
+var visit=("You have visited 18 times");
+alert(visit);
+
+document.write("My birth year is 2005 </br> Data type of my declared variable is number. </br></br></br> JohnDoe ordered 5 T-shirt(s) on XYZ Clothing store");
+
+
